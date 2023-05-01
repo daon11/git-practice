5b018186ae0e8b8d4f9599ba1904106f37135c0d
daon11 <encode97@gmail.com>
2023-05-01 21:51:55 +0900

    feat: Add items in lists (Trailing comma test)
    Let's see what happened.

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ def print_hello():
     animals = ['dog', 'cat', 'hamster']
     foods = [
             'Spagetti',
-            'Pizza'
+            'Pizza',
+            'bibimbob'
             ]
     names = [
             'Jone',
             'Jane',
             'Gil-dong',
+            'Dong-eun',
             ]
     for f_name in names:
         print(f'hello, {f_name}')
